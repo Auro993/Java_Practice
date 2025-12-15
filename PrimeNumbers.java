035a89677d09ea0be1616ef1a6764dbc7f3acd08
Aurosmita Sahoo <sahooaurosmita204@gmail.com>
2025-12-16 01:01:43 +0530

    Add files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,28 @@
+class PrimeNumbers
+{
+public static void main(String[]args)
+{
+int i=0;
+int num=0;
+String primenumbers="";
+
+for(i=1;i<=100;i++)
+{
+	int counter=0;
+	for(num=i;num>=1;num--)
+	{
+		if(i%num==0)
+		{
+			counter=counter+1;
+		}
+	}
+	if(counter==2)
+	{
+	PrimeNumbers= primenumbers + i +" ";
+	}
+}
+System.out.print("prime numbers from 1 to 100 are :");
+System.out.print(primenumbers);
+}
+}
+		
