a5b3e9433a95f996b69b20a449159a9e118e6e8d
Aurosmita Sahoo <sahooaurosmita204@gmail.com>
2025-12-18 21:27:49 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,10 @@
+public class Increment{  
+public static void main(String args[])
+{
+int a=3;
+System.out.print(a++);
+System.out.print(a--);
+System.out.print(++a);
+System.out.print(--a);
+}
+}	
