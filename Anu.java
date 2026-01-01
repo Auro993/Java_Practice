05ecc4689cbe4cd3abaa637fc064eb03cc53564c
Aurosmita Sahoo <sahooaurosmita204@gmail.com>
2026-01-01 18:17:02 +0530

    Add files via upload

---
+++ Anu.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Anu
+{
+public static void main(String[]args)
+{
+int a[]=new int[5];
+Scanner sc=new Scanner(System.in);
+for(int i=0;i<5;i++)
+{
+a[i]=sc.nextInt();
+}
+System.out.print(a[i]);
+}
+}
+
